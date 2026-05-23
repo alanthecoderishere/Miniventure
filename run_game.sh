c9cd25b6ece6f4d8566b1e980812fc36f7c833a1
alanthecoderishere <email-github-kamu@example.com>
2026-05-23 08:06:00 +0700

    Update konfigurasi skrip run_game

--- run_game.sh
+++ run_game.sh
@@ -2,5 +2,5 @@
 # compile and run script for macos
 set -e
 mkdir -p bin
-javac -d bin -cp "lib/*" $(find src -name "*.java")
+javac -cp "lib/*:src" src/com/miniv/core/Main.java -d bin
 java -XstartOnFirstThread --enable-native-access=ALL-UNNAMED -cp "bin:lib/*" com.miniv.core.Main
